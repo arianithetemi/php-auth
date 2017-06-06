85797ca49180385cceea13525daf2279ee5fee11
arianithetemi <nitihetemi@gmail.com>
2017-06-06 19:27:56 +0200

    Forms added, Frontend validation completed

--- index.php
+++ index.php
@@ -5,11 +5,93 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Simple PHP Auth</title>
+    <!-- css -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" />
+    <link href="css/parsley.css" rel="stylesheet" />
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <!-- js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/parsley.min.js"></script>
   </head>
   <body>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+          <div class="panel panel-login">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-6">
+                  <a href="#" class="active" id="login-form-link">Login</a>
+                </div>
+                <div class="col-xs-6">
+                  <a href="#" id="register-form-link">Register</a>
+                </div>
+              </div>
+              <hr>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-lg-12">
+                    <form id="login-form" action="" method="POST" role="form" style="display: block;">
+                      <div class="form-group">
+                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                      </div>
+                      <div class="form-group text-center">
+                        <input type="checkbox" tabindex="3" name="remember" id="remember">
+                        <label for="remember"> Remember Me</label>
+                      </div>
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-sm-6 col-sm-offset-3">
+                            <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+                          </div>
+                        </div>
+                      </div>
+                      <!--<div class="form-group">
+                        <div class="row">
+                          <div class="col-lg-12">
+                            <div class="text-center">
+                              <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>-->
+                    </form>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                    <form id="register-form" action="register.php" method="POST" role="form" data-parsley-validate style="display: none;">
+                      <div class="form-group">
+                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="" data-parsley-trigger="change" required>
+                      </div>
+                      <div class="form-group">
+                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="" data-parsley-trigger="change" required>
+                      </div>
+                      <div class="form-group">
+                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password" data-parsley-trigger="change" required data-parsley-minlength="6">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" name="confirm_password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password" data-parsley-trigger="change" required data-parsley-minlength="6" data-parsley-equalto="#password">
+                      </div>
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-sm-6 col-sm-offset-3">
+                            <input type="submit" name="register" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register">
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
